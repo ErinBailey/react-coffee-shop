c98be859d8b333fadc0aad20e197e42f3b7613d1
Hodges and Coulter <duedter@gmail.com>
2017-01-27 16:00:20 -0600

    adding buttons and ridiculous names

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const pictures = ['../images/coffee1.jpg','../images/coffee2.jpg','../images/coffee3.jpg', '../images/coffee4.jpg', '../images/coffee5.jpg', '../images/coffee6.jpg','../images/tombrady.jpg']
 
+const coffees = [ {picLink: '../images/coffee1.jpg', name: 'the youngest $$$'},{picLink:'../images/coffee2.jpg' , name: 'super extra powerful coffee'},{picLink: '../images/coffee3.jpg' , name: 'real man coffee'},{picLink: '../images/coffee4.jpg', name: 'girl coffee'},{picLink: '../images/coffee5.jpg', name: 'old coffee'},{picLink: '../images/coffee6.jpg', name: 'young coffee'},{picLink: '../images/tombrady.jpg', name: 'el baristo'}]
 
 
 const ItemComponent = React.createClass({
@@ -11,9 +12,18 @@ const ItemComponent = React.createClass({
 
     render(){
 
-      return React.createElement('div', null, React.createElement('img', {src:this.props.pictureLocation, 
-                                  style: {width:'400px', height:'400px'}}), React.createElement('br', null) )
-      
+      return React.createElement('div', null, React.createElement('img', {src:this.props.coffeeType.picLink, 
+                                  style: {width:'400px', height:'400px'}}), 
+                                  React.createElement('br', null), 
+                                  React.createElement('button',{
+                                    style:{ 
+                                      background:'#40FFB0', 
+                                      border: '2px', 
+                                      color: 'black' },
+                                    id:'homeButton'}, 'Add to cart'),
+                                    React.createElement('text', {style:{'font-size':'300%'}}," " + this.props.coffeeType.name ), 
+                                  React.createElement('br', null) )
+
     }
   })
 
@@ -28,8 +38,8 @@ const Main = React.createClass({
 
   render() {
     
-    const someList = pictures.map((item) => {
-      return (<ItemComponent pictureLocation={item}/>)
+    const someList = coffees.map((item) => {
+      return (<ItemComponent coffeeType={item}/>)
     });
 
     return React.createElement('div', null, React.createElement('h1', null, "The Coffee Shop"), 
